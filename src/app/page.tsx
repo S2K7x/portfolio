084ac0e7e518aac39a553a08edc57c5bd8d89709
S2K7x <166259891+S2K7x@users.noreply.github.com>
2025-03-15 23:04:26 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Page() {
 
       <section id="downloadCV" className="flex justify-center">
         <BlurFade delay={BLUR_FADE_DELAY * 2}>
-          <a href="/RAPHAELZERBIB.pdf" target="_blank" rel="noopener noreferrer">
+          <a href="/Shai_Krief2025.pdf" target="_blank" rel="noopener noreferrer">
             <Button className="flex gap-2 items-center px-6 py-6">
               <Download className="size-5" />
               <span>Download My Resume</span>
